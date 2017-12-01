7306f9839b816a62dabd0764bd04b3765458fde7
Aswin Karuvally <aswinbabuk@gmail.com>
2017-12-01 10:22:25 +0530

    added configuration file

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ you when it is low.
 ## Requirements  
 * dunst (A very lightweight notification daemon)
 * libnotify
+
+## Configuration  
+* Place the configuration file in your ~/.config  
+* notification are messages that can be show at whatever capacity user desires  
+* low_warning decides when to show battery low warning
+* critical_warning decides when to show battery critical warning
+* check_interval is the time between battery value checks
--- bali.py
+++ bali.py
@@ -3,19 +3,46 @@
 # Released under GNU General Public License
 
 # TODO
-# notification for each 5% drop from user confiurable point
 # constant notification and beep when battery below 5%
 # automatically shutdown or sleep system when critical battery
-# configuratoin file with sane defaults
-# readme file
+# coloured notifications
+# installation script
+# multi battery support
+# to cron or not to cron
+# also show battery status (charging/discharging/unknown)
 
 # import the serious stuff
 import argparse
 import subprocess
+import os
+
+
+# read and process configuration
+def read_configuration_file(configuration_file_path):
+	# essential variables
+	configuration_dictionary = {}
+	notification_list = []
+
+	# read file and generate dictionary
+	configuration_file = open(configuration_file_path, 'r')
+	for line in configuration_file:
+		if line[0] == '#' or not line.strip():
+			continue
+		else:
+			configuration = line.strip().split('=')
+			if configuration[0] == 'notification':
+				notification_list.append(int(configuration[1]))
+				configuration_dictionary['notifications'] = notification_list
+			else:
+				configuration_dictionary[configuration[0]] = int(configuration[1])
+
+	# close file and return data
+	configuration_file.close()
+	return configuration_dictionary
 
 
 # check the battery, issue warnings
-def check_battery(daemon=True, no_gui=False):
+def check_battery(configuration, daemon=True, no_gui=False):
 	# open the battery file
 	battery_file = open('/sys/class/power_supply/BAT0/capacity')
 	battery_value = battery_file.read().rstrip()
@@ -26,17 +53,35 @@ def check_battery(daemon=True, no_gui=False):
 			print('Battery is at ' + battery_value + '%')
 			return
 		subprocess.run(['notify-send', 'Battery is at ' + battery_value + '%'])
+		return
+		
+	# show user defined notification
+	for notification_level in configuration['notifications']:
+		if notification_level == battery_value:
+			subprocess.run(['notify-send', 'Battery is at ' + battery_value +
+				'%'])
+			return
 	
-	# if battery value is below 10 percent, alert
-	if int(battery_value) < 10:
+	# low battery alert
+	if int(battery_value) < configuration['low_warning']:
 		subprocess.run(['notify-send', 'Battery low! (at ' +
 			battery_value + '%)'])
+		return
+
+	# critical battery alert
+	if int(battery_value) < configuration['critical_warning']:
+		subprocess.run(['notify-send', 'Battery critical! (at ' +
+			battery_value + '%)'])
 	
-	return
-		
+	return	
 
 # the main function
 def main():
+	# read configuration data
+	user = os.getlogin()
+	configuration_dictionary = read_configuration_file('/home/' + user +
+		'/.config/bali.conf')
+	
 	# process command line arguments
 	parser = argparse.ArgumentParser(
 		description='The Battery Life monitor, alpha release')
@@ -52,9 +97,10 @@ def main():
 	# loop only if started in daemon mode
 	if arguments.daemon:
 		while True:
-			check_battery(daemon=True)
-			time.sleep(60)
+			check_battery(configuration=configuration_dictionary, daemon=True)
+			time.sleep(configuration_dictionary['check_interval'])
 	else:
-		check_battery(daemon=False, no_gui=arguments.nogui)
+		check_battery(configuration=configuration_dictionary, daemon=False,
+			no_gui=arguments.nogui)
 
 main()
